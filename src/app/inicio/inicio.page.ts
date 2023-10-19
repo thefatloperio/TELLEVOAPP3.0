dede646f705606105e1685ab21612df34394a914
thefatlopeiro <pe.barahona@duocuc.cl>
2023-10-19 01:53:41 -0300

    mostrar usuario

--- src/app/inicio/inicio.page.ts
+++ src/app/inicio/inicio.page.ts
@@ -7,12 +7,24 @@ import { AlertController, NavController } from '@ionic/angular';
   styleUrls: ['./inicio.page.scss'],
 })
 export class InicioPage implements OnInit {
+  usuario: any;
 
   constructor(public navCtrl: NavController,
     public alertController : AlertController) { }
 
   ngOnInit() {
+    const usuarioString = localStorage.getItem('usuario');
+    if (usuarioString) {
+      this.usuario = JSON.parse(usuarioString);
+      // Ahora puedes acceder a las propiedades específicas del objeto de usuario
+      const nombre = this.usuario.nombre;
+      const contraseña = this.usuario.contraseña;
+      const repitecontraseña = this.usuario.repitecontraseña;
+      console.log('Nombre del usuario:', nombre);
+      console.log('Email del usuario:', contraseña);
+      console.log('Tipo del usuario:', repitecontraseña);
   }
+}
 
   user = localStorage.getItem('usuario')
   
